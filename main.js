d0dfb6383956f2d002f65a84ad04402163a652ec
sdclibbery <sdclibbery@googlemail.com>
2016-01-06 21:46:48 +0000

    basic webgl canvas setup

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ define(function(require) {
 
 //  var play = require('play/play');
 
+var canvas = document.getElementById("canvas");
+var ctxGl = canvas.getContext("webgl");
+if (!ctxGl) { ctxGl = canvas.getContext("experimental-webgl"); }
+if (!ctxGl) { document.getElementById('info').innerHTML = 'WebGL not supported!'; }
+
+ctxGl.clearColor(0, 0, 0, 1);
+ctxGl.enable(ctxGl.DEPTH_TEST);
+ctxGl.depthFunc(ctxGl.LEQUAL);
+ctxGl.clear(ctxGl.COLOR_BUFFER_BIT|ctxGl.DEPTH_BUFFER_BIT);
+
 });
