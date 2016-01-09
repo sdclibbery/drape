63ecc95f20f639cc67798537e70020afd48017cb
sdclibbery <sdclibbery@googlemail.com>
2016-01-09 23:17:29 +0000

    Simple heightmap ready for better domain modelling

--- height.js
+++ height.js
@@ -1,8 +1,7 @@
 define(function(require) {
 
 return function (x, y) {
-  if (x*x + y*y > 300) { return 0; }
-  return Math.sin(x) + Math.sin(y);
+  return Math.max(10 - 0.03*(x*x + y*y), 0);
 };
 
 });
