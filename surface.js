8a69b9af7a09f032e8764d5104aede9710953d3d
sdclibbery <sdclibbery@googlemail.com>
2016-01-10 16:26:09 +0000

    add a sphere function
    not complete yet; need tangent and curvature

--- surface.js
+++ surface.js
@@ -14,7 +14,7 @@ var inf = Infinity;
 var bottom = function (x, y) {
   return {
     pos: new Vector(x,y,0),
-    norm: new Vector(0,0,0) // No surface here!
+    norm: new Vector(0,0,1)
   };
 }
 
@@ -29,17 +29,35 @@ var drape = function (items) {
 
 var cube = function (size) {
   return function (x, y) {
-    if (abs(x) <= size/2 && abs(y) <= size/2) {
-//      var nearestToY = abs(x) > abs(y);
-      return {
-        pos: new Vector(x,y,size),
-        norm: new Vector(0,0,1),
-//        tangent: new Vector(0,0, 0),
-        tangentCurvature: 0,
-        cotangentCurvature: 0
-      };
-    }
-    return bottom(x,y);
+    if (abs(x) > size/2 || abs(y) > size/2) { return bottom(x,y); }
+    var nearestToY = abs(x) > abs(y);
+    var tanX = nearestToY ? 0 : 1;
+    var tanY = nearestToY ? 1 : 0;
+    return {
+      pos: new Vector(x,y,size),
+      norm: new Vector(0,0,1),
+      tangent: new Vector(tanX, tanY, 0),
+      tangentCurvature: 0,
+      cotangentCurvature: 0
+    };
+  };
+};
+
+var sphere = function (radius) {
+  var r2 = radius*radius;
+  return function (x, y) {
+    var x2 = x*x;
+    var y2 = y*y;
+    if (x2 + y2 > r2) { return bottom(x,y); }
+    var pos = new Vector(x, y, sqrt(r2 - x2 - y2));
+    return {
+      pos: pos,
+      norm: pos.unit(),
+// ! need to sort these and think about testing. How about some property based testing? eg test norm is perp to tan etc
+//      tangent: new Vector(),
+//      tangentCurvature: ,
+//      cotangentCurvature: 
+    };
   };
 };
 
@@ -100,7 +118,8 @@ var sweep = function (path, profile) {
 };
 */
 return drape([
-              cube(10)
+//              cube(10)
+              sphere(10)
 //              rotate(PI/4, cube(10))
 //              translate(5, 5, cube(12))
 //              sweep(line(-10,-10, 10,10), ellipse(3, 5))
