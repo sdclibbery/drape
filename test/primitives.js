a8c73490c73c1bb23828b39063ce6b18b712fa79
sdclibbery <sdclibbery@googlemail.com>
2016-01-11 23:57:58 +0000

    refactor property test

--- test/primitives.js
+++ test/primitives.js
@@ -7,18 +7,18 @@ var assert = function (surf, pred, m) {
   if (!pred(surf)) { console.log(m+' at '+surf.pos.x.toFixed(3)+', '+surf.pos.y.toFixed(3)+', '+surf.pos.z.toFixed(3)); }
 };
 
-var propertyTests = function (surf) {
-  assert(surf, function(surf) { return surf.norm.perpTo(surf.tangent); } , 'norm _|_ tangent');
-};
-
-return function () {
+var testCube = function () {
   console.log('cube(1)');
   var cube = prim.cube(1);
   for (var i=0; i<100; i++) {
     var x = 1.1*(Math.random()-0.5);
     var y = 1.1*(Math.random()-0.5);
-    propertyTests(cube(x,y));
+    assert(cube(x,y), s => s.norm.perpTo(s.tangent), 'norm _|_ tangent');
   }
+};
+
+return function () {
+  testCube();
 };
 
 });
