44539d879f799de4aba89eaccd496ae0e02bb96d
sdclibbery <sdclibbery@googlemail.com>
2016-01-12 00:06:01 +0000

    planning for property tests

--- test/primitives.js
+++ test/primitives.js
@@ -13,9 +13,21 @@ var testCube = function () {
   for (var i=0; i<100; i++) {
     var x = 1.1*(Math.random()-0.5);
     var y = 1.1*(Math.random()-0.5);
+    // z zero outside cube
+    // z one inside cube
+    // normal always points up
     assert(cube(x,y), s => s.norm.perpTo(s.tangent), 'norm _|_ tangent');
+    // tangent...
+    // curvature always zero
   }
 };
+
+// Sphere
+ // norm _|_ tangent
+ // norm is direction to point
+ // tangent is always pointing around the sphere (z cpt is zero)
+ // pos distance from ctr pt is always radius
+ // curvature..?
 
 return function () {
   testCube();
