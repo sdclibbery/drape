67eeef64eb884a09d3c861607833250ed09c56d9
Stephen Clibbery <sdclibbery@googlemail.com>
2016-01-17 21:01:59 +0000

    Add arrow heads to cut dir visualisation

--- cutlines.js
+++ cutlines.js
@@ -13,28 +13,36 @@ return function (surface) {
   var resY = 50;
   var size = 0.5;
 
-  var numVtxs = resX*resY*2;
+  var numVtxs = resX*resY*4;
   var vtxPosns = new Float32Array(numVtxs*3);
   for (var y = 0; y < resY; y++) {
     for (var x = 0; x < resX; x++) {
-      var v = (x + y*resX) * 6;
+      var v = (x + y*resX) * 12;
       var posx = (x - resX/2)*size/resX;
       var posy = (y - resY/2)*size/resY;
       var s = surface(posx, posy);
       s.pos.z += 0.003;
-      gcodeToGl(vtxPosns, v, s.pos);
-      gcodeToGl(vtxPosns, v+3, s.pos.add(s.cutDir.multiply(size*0.8/resX)));
+      var len = size*0.8/resX;
+      var perp = s.norm.cross(s.cutDir);
+      gcodeToGl(vtxPosns, v, s.pos.add(s.cutDir.multiply(-len*0.5)));
+      gcodeToGl(vtxPosns, v+3, s.pos.add(s.cutDir.multiply(len*0.5)));
+      gcodeToGl(vtxPosns, v+6, s.pos.add(s.cutDir.multiply(len*0.4)).add(perp.multiply(len*0.3)));
+      gcodeToGl(vtxPosns, v+9, s.pos.add(s.cutDir.multiply(len*0.4)).add(perp.multiply(-len*0.3)));
     }
   }
 
-  var numIndices = resX*resY*2;
+  var numIndices = resX*resY*6;
   var indexes = new Uint16Array(numIndices);
   for (var y = 0; y < resY; y++) {
     for (var x = 0; x < resX; x++) {
-      var i = (x + y*resX) * 2;
-      var v = i;
+      var i = (x + y*resX) * 6;
+      var v = (x + y*resX) * 4;
       indexes[i+0] = v;
       indexes[i+1] = v+1;
+      indexes[i+2] = v+1;
+      indexes[i+3] = v+2;
+      indexes[i+4] = v+1;
+      indexes[i+5] = v+3;
     }
   }
 
