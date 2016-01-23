7ae67598cce9a9f21549e77f910770093585e95d
Stephen Clibbery <sdclibbery@googlemail.com>
2016-01-23 19:52:53 +0000

    support multitouch

--- touch.js
+++ touch.js
@@ -4,44 +4,56 @@ var touch = {
   pan: function () {},
 };
 
-var touchx, touchy;
-var touchStart = function (touches) {
-  touchx = touches[0].x;
-  touchy = touches[0].y;
-};
-var touchMove = function (touches) {
-	touch.pan(touches[0].x - touchx, touches[0].y - touchy);
-	touchx = touches[0].x;
-  touchy = touches[0].y;
-};
-var touchEnd = function () {}
-
+var touches = {};
 window.addEventListener("touchstart", function (evt) {
   evt.preventDefault();
-  touchStart([{x: evt.changedTouches[0].clientX, y: evt.changedTouches[0].clientY}]);
+  toArray(evt.changedTouches).map(function (touch) {
+    touches[touch.identifier] = { id: touch.identifier, x: touch.clientX, y: touch.clientY };
+  });
 });
 window.addEventListener("touchmove", function (evt) {
   evt.preventDefault();
-  touchMove([{x: evt.changedTouches[0].clientX, y: evt.changedTouches[0].clientY}]);
+  toArray(evt.changedTouches).map(function (changed) {
+    var t = touches[changed.identifier];
+    var shouldPan = Object.keys(touches).length == 1;
+    if (shouldPan) {
+      touch.pan(changed.clientX - t.x, changed.clientY - t.y);
+    }
+    t.x = changed.clientX;
+    t.y = changed.clientY;
+  });
 });
 window.addEventListener("touchend", function (evt) {
   evt.preventDefault();
-  touchEnd();
+  toArray(evt.changedTouches).map(function (touch) {
+    delete touches[touch.identifier];
+  });
 });
 
 var mousedown = false;
+var mx, my;
 window.onmousedown = function (evt) {
   mousedown = true;
-  touchStart([{x: evt.clientX, y: evt.clientY}]);
+  mx = evt.clientX;
+  my = evt.clientY;
 };
 window.onmousemove = function (evt) {
   if (mousedown) {
-    touchMove([{x: evt.clientX, y: evt.clientY}]);
+    touch.pan(evt.clientX - mx, evt.clientY - my);
+    mx = evt.clientX;
+    my = evt.clientY;
   }
 };
 window.onmouseup = function (evt) {
-  touchEnd();
   mousedown = false;
+};
+
+var toArray = function (a) {
+  var r = [];
+  for (var i=0; i<a.length; i++) {
+    r.push(a[i]);
+  }
+  return r;
 };
 
 return touch;
