059d21bc1fbe30b258a72c7dc92b694dc24725ce
Stephen Clibbery <sdclibbery@googlemail.com>
2016-02-04 23:54:20 +0000

    refactor primitive tests

--- test/primitives.js
+++ test/primitives.js
@@ -13,49 +13,49 @@ var assert = function (surf, pred, m) {
   if (!pred(surf)) { console.log(m+' at ' + v2s(surf.pos) + ' cut: ' + v2s(surf.cutDir)); }
 };
 
-var testSuites = {};
-
-var commonProperies = function (surf) {
+var commonProperties = function (surf) {
   assert(surf, s => s.norm.perpTo(s.cutDir), 'norm _|_ cutDir');
   assert(surf, s => s.norm.isUnit(), 'norm is unit');
   assert(surf, s => s.cutDir.isUnit(), 'cutDir is unit');
   assert(surf, s => s.pos.z == 0 || s.cutDir.cross(s.pos).z < 0, 'cutDir is clockwise');
 };
 
-testSuites.cube = function () {
+var test = function (surface, customProperties) {
   for (var i=0; i<100; i++) {
     var x = 1.1*(Math.random()-0.5);
     var y = 1.1*(Math.random()-0.5);
-    var surf = prim.cube(1)(x,y);
-    commonProperies(surf);
+    var surf = surface(x,y);
+    commonProperties(surf);
+    if (surf.pos.z > 0) {
+      customProperties(surf);
+    }
+  }
+};
+
+var testSuites = {};
+
+testSuites.cube = function () {
+  test(prim.cube(1), function (surf) {
     assert(surf, s => s.norm.z === 1, 'normal always points up');
     assert(surf, s => abs(s.cutDir.x) === 0 || abs(s.cutDir.y) === 0, 'cutDir always on an axis');
     assert(surf, s => s.cutCurvature === 0, 'curvature is zero');
     assert(surf, s => s.perpCurvature === 0, 'perp curvature is zero');
-  }
+  });
 };
 
 testSuites.sphere = function () {
-  for (var i=0; i<100; i++) {
-    var x = 1.1*(Math.random()-0.5);
-    var y = 1.1*(Math.random()-0.5);
-    var surf = prim.sphere(1)(x,y);
-    commonProperies(surf);
+  test(prim.sphere(1), function (surf) {
     assert(surf, s => s.cutDir.z === 0, 'cutDir always in plane');
     assert(surf, s => s.pos.cross(s.norm).isZero(), 'norm in same direction as position');
     assert(surf, s => s.pos.isUnit(), 'unit radius');
     assert(surf, s => s.cutCurvature == 1/1, 'cutDir curvature is 1/radius');
     assert(surf, s => s.perpCurvature == 1/1, 'perp curvature is 1/radius');
-  }
+  });
 };
 
 testSuites.lineSweptEllipse = function () {
-  for (var i=0; i<100; i++) {
-    var x = 1.1*(Math.random()-0.5);
-    var y = 1.1*(Math.random()-0.5);
-    var surf = prim.sweep(prim.line(-1,0, 1,0), prim.ellipse(1, 1))(x,y);
-    commonProperies(surf);
-  }
+  test(prim.sweep(prim.line(-1,0, 1,0), prim.ellipse(1, 1)), function (surf) {
+  });
 };
 
 return function () {
