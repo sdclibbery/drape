97a80f29cdfb238d93be1ae9b574a0975fa4b9ee
Stephen Clibbery <sdclibbery@googlemail.com>
2016-02-23 23:09:50 +0000

    Numerical curvature tests

--- test/primitives.js
+++ test/primitives.js
@@ -1,9 +1,12 @@
 define(function(require) {
 
-var prim = require('modelling/primitives');
 var vector = require('vector');
+var prim = require('modelling/primitives');
 
 var abs = Math.abs;
+var sqrt = Math.sqrt;
+var sqr = x => x*x;
+var sin = Math.sin;
 
 var v2s = function (v) {
   return '(' + v.x.toFixed(3) + ',' + v.y.toFixed(3) + ','+v.z.toFixed(3) + ')';
@@ -20,23 +23,42 @@ var commonProperties = function (surf) {
   assert(surf, s => s.pos.z == 0 || s.cutDir.cross(s.pos).z < 0, 'cutDir is clockwise');
 };
 
+var deltaDistance = 1e-5;
+var delta = function (surface, surf, dir) {
+  var hdir = new vector(dir.x, dir.y, 0).unit();
+  var pos = surf.pos.add(hdir.multiply(deltaDistance));
+  return surface(pos.x, pos.y);
+}
+
+var nearTo = function (v1, v2, eps) {
+  return abs(v2-v1) < eps;
+}
+
 var calcNormal = function (s, sc, sp) {
   var c = sc.pos.subtract(s.pos);
   var p = sp.pos.subtract(s.pos);
   return p.cross(c).unit();
 };
 
-var delta = function (surface, surf, dir) {
-  var pos = surf.pos.add(dir.multiply(1e-6));
-  return surface(pos.x, pos.y);
-}
+var calcCurvature = function (sn, s, sp) {
+  var x1 = -deltaDistance;
+  var y1 = sn.pos.z;
+  var y2 = s.pos.z;
+  var x3 = deltaDistance;
+  var y3 = sp.pos.z;
+  return (2*abs(x1*y2 + x3*y1 - x1*y3 - x3*y2)) / sqrt((sqr(x1) + sqr(y2-y1)) * (sqr(x3) + sqr(y2-y3)) * (sqr(x3-x1) + sqr(y3-y1)));
+};
 
 var numericalTests = function (surface, surf) {
-  var sc = delta(surface, surf, surf.cutDir);
-  var sp = delta(surface, surf, surf.cutDir.cross(surf.norm));
-  assert(surf, s => s.norm.nearTo(calcNormal(s, sc, sp), 1e-6), 'normal is close to calculated normal'+surf.norm.dot(calcNormal(surf, sc, sp)));
-  // Test cutDir curvature
-  // Test perp curvature
+  var c = surf.cutDir;
+  var p = surf.cutDir.cross(surf.norm);
+  var sc = delta(surface, surf, c);
+  var sp = delta(surface, surf, p);
+  assert(surf, s => s.norm.nearTo(calcNormal(s, sc, sp), 1e-4), 'normal is close to calculated normal');
+  var scn = delta(surface, surf, c.negative());
+  var spn = delta(surface, surf, p.negative());
+  assert(surf, s => nearTo(s.cutCurvature, calcCurvature(scn, s, sc), 1e-4), 'cutCurvature is close to calculated cutCurvature '+surf.cutCurvature+' '+calcCurvature(scn, surf, sc));
+  assert(surf, s => nearTo(s.perpCurvature, calcCurvature(spn, s, sp), 1e-4), 'perpCurvature is close to calculated perpCurvature '+surf.perpCurvature+' '+calcCurvature(spn, surf, sp));
 };
 
 var test = function (surface, customProperties) {
@@ -67,7 +89,7 @@ testSuites.sphere = function () {
   test(prim.sphere(1), function (surf) {
     assert(surf, s => s.cutDir.z === 0, 'cutDir always in plane');
     assert(surf, s => s.pos.cross(s.norm).isZero(), 'norm in same direction as position');
-    assert(surf, s => s.pos.isUnit(), 'unit radius');
+    assert(surf, s => s.pos.isUnit(), 'radius');
     assert(surf, s => s.cutCurvature == 1/1, 'cutDir curvature is 1/radius');
     assert(surf, s => s.perpCurvature == 1/1, 'perp curvature is 1/radius');
   });
