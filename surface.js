2b60f54bae2f6f53c44e80488d829a80977a12b5
Stephen Clibbery <sdclibbery@googlemail.com>
2016-04-30 12:17:59 +0100

    Remove bezier end caps

--- surface.js
+++ surface.js
@@ -21,8 +21,8 @@ return csg.union([
 //              translate(0.15, 0.15, prim.cube(0.1)),
 //              translate(0.12, 0.12, prim.sphere(0.09)),
 //              translate(0.1, 0.1, prim.cube(0.1)),
-              prim.sweep(line(0,0.2, 0,-0.2), ellipse(0.07, 0.07), power(0.25)),
-//              prim.sweep(bezier(0,0.2, 0.2,0, 0,0, 0,-0.2), ellipse(0.05, 0.07), power(0.2)),
+//              prim.sweep(line(0,0.2, 0,-0.2), ellipse(0.07, 0.07), power(0.25)),
+              prim.sweep(bezier(0,0.2, 0.2,0, 0,0, 0,-0.2), ellipse(0.05, 0.07), power(0.25)),
             ]);
 
 });
