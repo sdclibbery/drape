c81bc854d0631ba37d49dcbf1a52239880d6d285
Stephen Clibbery <sdclibbery@googlemail.com>
2016-05-21 18:15:02 +0100

    generate criss cross toolpath

--- cnc/toolpath.js
+++ cnc/toolpath.js
@@ -3,15 +3,21 @@ define(function(require) {
 var vector = require('vector');
 
 return function (surface) {
-  return [
-    { pos: new vector(0, 0, 0), },
-    { pos: new vector(0.1, 0, 0), },
-    { pos: new vector(0.1, 0.1, 0), },
-    { pos: new vector(0.2, 0.1, 0), },
-    { pos: new vector(0.2, 0.2, 0), },
-    { pos: new vector(0.3, 0.2, 0), },
-    { pos: new vector(0.3, 0.3, 0), },
-  ];
+  var resX = 50;
+  var resY = 50;
+  var size = 0.5;
+
+  var nodes = [];
+  for (var y = 0; y < resY; y++) {
+    for (var xl = 0; xl < resX; xl++) {
+      if (y%2) { x = resX - 1 - xl; } else { x = xl; }
+      var posx = (x - resX/2)*size/resX;
+      var posy = (y - resY/2)*size/resY;
+      nodes.push({ pos: surface(posx, posy).pos, });
+    }
+  }
+
+  return nodes;
 };
 
 });
