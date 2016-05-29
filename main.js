1eb167d0712db3c25a9cfb7dcb36c41bf1655a83
Stephen Clibbery <sdclibbery@googlemail.com>
2016-05-29 09:09:14 +0100

    Move gcode to own module

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var mouse = require('input/mouse');
 var camera = require('camera');
 var surface = require('surface');
 var toolpath = require('cnc/toolpath');
+var gcode = require('cnc/gcode');
 
 var canvas = document.getElementById("canvas");
 function onResize() {
@@ -36,32 +37,7 @@ var render = function () {
 render();
 
 window.export = function () {
-  var gcode = "";
-  gcode += '(DIAMOND, CIR, SQ TEST PROGRAM)\n';
-  gcode += '(FEB-08-12, 12:05) \n';
-  gcode += '( *** UNPROVEN PROGRAM *** )\n';
-  gcode += '( RUN IN VISE ON PARALLELS )\n';
-  gcode += '(Z OFFSET: TOP OF MATERIAL WITH )\n';
-  gcode += '( 0.375" MATERIAL ABOVE VISE JAWS )\n';
-  gcode += '(X0,Y0,Z0= Center, Center, Top)\n';
-  gcode += '(STOCK ORIGIN = X0. Y0. Z.01)\n';
-  gcode += '(MATERIAL TYPE= ALUMINUM inch - 6061)\n';
-  gcode += '(MATERIAL SIZE= X1.75 Y1.75 Z.5)\n';
-  gcode += '(TOOL= 1/4 2-FLUTE HSS END MILL)";\n';
-  gcode += 'G17 G20 G90 G94 G54\n';
-  gcode += 'G17 G20 G90 G94 G54\n';
-  gcode += 'G0 Z0.25\n';
-  gcode += 'X-0.5 Y0.\n';
-  gcode += 'Z0.1\n';
-  gcode += 'G01 Z0. F5.\n';
-  gcode += 'G02 X0. Y0.5 I0.5 J0. F2.5\n';
-  gcode += 'X0.5 Y0. I0. J-0.5\n';
-  gcode += 'X0. Y-0.5 I-0.5 J0.\n';
-  gcode += 'X-0.5 Y0. I0. J0.5\n';
-  gcode += 'G01 Z0.1 F5.\n';
-  gcode += 'G00 X0. Y0. Z0.25\n';
-
-  newWindow = window.open("data:text/plain," + encodeURIComponent(gcode), "_blank");
+  newWindow = window.open("data:text/plain," + encodeURIComponent(gcode(toolpath)), "_blank");
   newWindow.document.title = "draped.gcode";
   newWindow.focus();
 }
