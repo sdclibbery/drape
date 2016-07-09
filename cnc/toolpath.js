bea6e44f68e87af1ba581ac007f5907665d564a9
Stephen Clibbery <sdclibbery@googlemail.com>
2016-07-09 17:55:43 +0100

    first stab at toolpath property tests. failing!

--- cnc/toolpath.js
+++ cnc/toolpath.js
@@ -3,21 +3,20 @@ define(function(require) {
 var vector = require('vector');
 
 return function (surface) {
-  var resX = 60;
-  var resY = 60;
+  var tp = [];
+  var res = 60;
+  tp.toolRadius = surface.size / res;
   var size = surface.size;
 
-  var nodes = [];
-  for (var y = 0; y < resY; y++) {
-    for (var xl = 0; xl < resX; xl++) {
-      if (y%2) { x = resX - 1 - xl; } else { x = xl; }
-      var posx = (x - resX/2)*size/resX;
-      var posy = (y - resY/2)*size/resY;
-      nodes.push({ pos: surface(posx, posy).pos, });
+  for (var y = 0; y < res; y++) {
+    for (var xl = 0; xl < res; xl++) {
+      if (y%2) { x = res - 1 - xl; } else { x = xl; }
+      var posx = (x - res/2)*size/res;
+      var posy = (y - res/2)*size/res;
+      tp.push({ pos: surface(posx, posy).pos, });
     }
   }
-
-  return nodes;
+  return tp;
 };
 
 });
--- test/toolpath.js
+++ test/toolpath.js
@@ -2,11 +2,66 @@ define(function(require) {
 
 var toolpath = require('cnc/toolpath');
 var vector = require('vector');
+var prim = require('modelling/primitives');
+
+var distanceToLine = function (p, a, b) {
+  var line = b.subtract(a);
+  var dir = line.unit();
+  var pa = p.subtract(a);
+  var param = pa.dot(dir);
+  if (param <= 0) { return pa.length(); }
+  if (param >= line.length()) { return p.subtract(b).length(); }
+  var along = dir.multiply(param);
+  var perp = pa.subtract(along);
+  return perp.length();
+};
+
+var distanceTo = function (p, toolpath) {
+  var best = Infinity;
+  toolpath.map(function (node, idx) {
+    if (idx === 0) { return; }
+    var a = toolpath[idx-1].pos;
+    var b = node.pos;
+    best = Math.min(best, distanceToLine(p, a, b));
+  });
+  return best;
+};
+
+var expect = function (actual, id) {
+  return { toBe: function (matcher) {
+    var r = matcher(actual);
+    if (!r.result) {
+      console.log("!", id, " failed!");
+      console.log("  ", r.msg);
+    }
+  } };
+};
+var equalTo = e => (a => ({ result: (a === e), msg: 'expected '+a+' to be '+e }));
+var lessThan = e => (a => ({ result: (a < e), msg: 'expected '+a+' to be less than '+e }));
 
 var tests = {};
 
-tests.allPointsAreCoveredByToolpath = function () {
+tests.allPointsOnSurfaceAreCoveredByToolpath = function () {
+  var surface = prim.cube(0.5);
+  surface.size = 0.6;
+  var tp = toolpath(surface);
+  for (var i=0; i<100; i++) {
+    var x = 1*(Math.random()-0.5);
+    var y = 1*(Math.random()-0.5);
+    var distance = distanceTo(new vector(x, y), tp);
+    expect(distance, x+','+y).toBe(lessThan(tp.toolRadius));
+  }
 }
+
+tests.distanceToLine = function () {
+  var v = (x,y) => new vector(x,y);
+  expect(distanceToLine(v(1,1), v(2,1), v(5,1)), 't1').toBe(equalTo(1));
+  expect(distanceToLine(v(2,1), v(2,1), v(5,1)), 't2').toBe(equalTo(0));
+  expect(distanceToLine(v(3,1), v(2,1), v(5,1)), 't3').toBe(equalTo(0));
+  expect(distanceToLine(v(3,0), v(2,1), v(5,1)), 't4').toBe(equalTo(1));
+  expect(distanceToLine(v(5,1), v(2,1), v(5,1)), 't5').toBe(equalTo(0));
+  expect(distanceToLine(v(6,1), v(2,1), v(5,1)), 't6').toBe(equalTo(1));
+};
 
 return function () {
   for (name in tests) {
