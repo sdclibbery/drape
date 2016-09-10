485e271e67c052310bba134494909caf80b0f8cb
Stephen Clibbery <sdclibbery@googlemail.com>
2016-09-10 23:57:46 +0100

    replan and start to find maximum

--- cnc/toolpath.js
+++ cnc/toolpath.js
@@ -4,18 +4,28 @@ var vector = require('vector');
 
 return function (surface, tool) {
   var tp = [];
-  const size = surface.size;
-  const res = Math.floor(size / tool.radius);
+  var max = gridSampledMaximum(surface, new vector(0,0), surface.size);
 
-  for (var y = 0; y <= res; y++) {
-    for (var xl = 0; xl <= res; xl++) {
-      if (y%2) { x = res - xl; } else { x = xl; }
-      var posx = (x - res/2)*size/res;
-      var posy = (y - res/2)*size/res;
-      tp.push({ pos: surface(posx, posy).pos, });
-    }
-  }
+tp.push({ pos: surface(max.x, max.y).pos, });
+tp.push({ pos: surface(max.x, max.y).pos.add(surface(max.x, max.y).cutDir), });
+
   return tp;
 };
+
+function gridSampledMaximum (surface, ctr, size) {
+  const res = 20;
+  var maximum = new vector(0,0,0);
+  for (var y = 0; y <= res; y++) {
+    for (var x = 0; x <= res; x++) {
+      var posx = ctr.x + (x - res/2)*size/res;
+      var posy = ctr.y + (y - res/2)*size/res;
+      const pos = surface(posx, posy).pos;
+      if (pos.z > maximum.z) {
+        maximum = pos;
+      }
+    }
+  }
+  return maximum;
+}
 
 });
