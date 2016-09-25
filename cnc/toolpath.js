421db40350d2727ab4eff65991ebd0614b132193
Stephen Clibbery <sdclibbery@googlemail.com>
2016-09-25 23:51:41 +0100

    Restart with grid based toolpath generation

--- cnc/toolpath.js
+++ cnc/toolpath.js
@@ -4,33 +4,24 @@ var vector = require('vector');
 
 return function (surface, tool) {
   var tp = [];
-  var max = gridSampledMaximum(surface, new vector(0,0), surface.size, 10);
-  max = gridSampledMaximum(surface, max.as2D(), surface.size/10, 10);
 
-  var s = surface(max.x, max.y);
-  for (var i = 0; i < 500; i++) {
-    if (s.isBottom) { break; }
-    tp.push({ pos: s.pos, });
-    var newPos = s.pos.add(s.cutDir.multiply(6));
-    s = surface(newPos.x, newPos.y);
-  }
+  grid(new vector(0,0), surface.size, tool.radius)
+    .map(v => surface(v.x, v.y))
+    .filter(s => !s.isBottom)
+    .map(s => tp.push({ pos: s.pos, }));
 
   return tp;
 };
 
-function gridSampledMaximum (surface, ctr, size, res) {
-  var maximum = new vector(0,0,0);
-  for (var y = 0; y <= res; y++) {
-    for (var x = 0; x <= res; x++) {
-      var posx = ctr.x + (x - res/2)*size/res;
-      var posy = ctr.y + (y - res/2)*size/res;
-      const pos = surface(posx, posy).pos;
-      if (pos.z > maximum.z) {
-        maximum = pos;
-      }
+function grid (ctr, size, step) {
+  const hsize = size/2;
+  var result = [];
+  for (var x = ctr.x-hsize; x <= ctr.x+hsize; x+=step) {
+    for (var y = ctr.y-hsize; y <= ctr.y+hsize; y+=step) {
+      result.push(new vector(x, y));
     }
   }
-  return maximum;
+  return result;
 }
 
 });
